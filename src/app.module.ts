eece94d146e6de02a60f26748919d2f50d0c3309
Twelve <ntngocwd96@gmail.com>
2023-03-23 08:03:46 +0700

    feat(dev-env): add prettier to git hooks

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,30 +6,30 @@ import { AppService } from './app.service';
 import { database_config } from './configs/configuration.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test', 'provision')
-          .default('development'),
-        PORT: Joi.number().port().required(),
-        DATABASE_PORT: Joi.number().port().required(),
-        DATABASE_USERNAME: Joi.string().min(4).required(),
-        DATABASE_PASSWORD: Joi.string().min(4).required(),
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_URI: Joi.string().required(),
-      }),
-      validationOptions: {
-        abortEarly: false,
-      },
-      load: [database_config],
-      isGlobal: true,
-      cache: true,
-      expandVariables: true,
-      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				NODE_ENV: Joi.string()
+					.valid('development', 'production', 'test', 'provision')
+					.default('development'),
+				PORT: Joi.number().port().required(),
+				DATABASE_PORT: Joi.number().port().required(),
+				DATABASE_USERNAME: Joi.string().min(4).required(),
+				DATABASE_PASSWORD: Joi.string().min(4).required(),
+				DATABASE_HOST: Joi.string().required(),
+				DATABASE_URI: Joi.string().required(),
+			}),
+			validationOptions: {
+				abortEarly: false,
+			},
+			load: [database_config],
+			isGlobal: true,
+			cache: true,
+			expandVariables: true,
+			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger(bootstrap.name);
-  const app = await NestFactory.create(AppModule);
-  const config_service = app.get(ConfigService);
-  await app.listen(config_service.get('PORT'), () =>
-    logger.log(`Application running on port ${config_service.get('PORT')}`),
-  );
+	const logger = new Logger(bootstrap.name);
+	const app = await NestFactory.create(AppModule);
+	const config_service = app.get(ConfigService);
+	await app.listen(config_service.get('PORT'), () =>
+		logger.log(`Application running on port ${config_service.get('PORT')}`),
+	);
 }
 bootstrap();
