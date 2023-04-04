079cb4781555912f1c2972a978ddd034923c28f6
Twelve <ntngocwd96@gmail.com>
2023-04-04 10:06:27 +0700

    feat(serialization): add class transformer

---
+++ src/interceptors/mongoose-class-serializer.interceptor.ts
@@ -0,0 +1,50 @@
+import {
+	ClassSerializerInterceptor,
+	PlainLiteralObject,
+	Type,
+} from '@nestjs/common';
+import { ClassTransformOptions, plainToClass } from 'class-transformer';
+import { Document } from 'mongoose';
+
+function MongooseClassSerializerInterceptor(
+	classToIntercept: Type,
+): typeof ClassSerializerInterceptor {
+	return class Interceptor extends ClassSerializerInterceptor {
+		private changePlainObjectToClass(document: PlainLiteralObject) {
+			if (!(document instanceof Document)) {
+				return document;
+			}
+			return plainToClass(classToIntercept, document.toJSON());
+		}
+
+		private prepareResponse(
+			response:
+				| PlainLiteralObject
+				| PlainLiteralObject[]
+				| { items: PlainLiteralObject[]; count: number },
+		) {
+			if (!Array.isArray(response) && response?.items) {
+				const items = this.prepareResponse(response.items);
+				return {
+					count: response.count,
+					items,
+				};
+			}
+
+			if (Array.isArray(response)) {
+				return response.map(this.changePlainObjectToClass);
+			}
+
+			return this.changePlainObjectToClass(response);
+		}
+
+		serialize(
+			response: PlainLiteralObject | PlainLiteralObject[],
+			options: ClassTransformOptions,
+		) {
+			return super.serialize(this.prepareResponse(response), options);
+		}
+	};
+}
+
+export default MongooseClassSerializerInterceptor;
--- src/modules/shared/base/base.entity.ts
+++ src/modules/shared/base/base.entity.ts
@@ -1,6 +1,10 @@
 import { Prop } from '@nestjs/mongoose';
+import { Transform } from 'class-transformer';
 
 export class BaseEntity {
+	@Transform((value) => value.obj._id.toString())
+	_id?: string;
+
 	@Prop({ default: null })
 	deleted_at?: Date;
 }
