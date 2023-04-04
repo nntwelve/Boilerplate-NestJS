0cec2c867ce5195a7a394958fc5ce121983a5ce1
Twelve <ntngocwd96@gmail.com>
2023-04-04 10:10:00 +0700

    feat(user): override create user with role

--- src/modules/users/interfaces/users.interface.ts
+++ src/modules/users/interfaces/users.interface.ts
@@ -1,4 +1,4 @@
 import { User } from '@modules/users/entities/user.entity';
 import { BaseRepositoryInterface } from '@repositories/base/base.interface.repository';
 
-export type UserRepositoryInterface = BaseRepositoryInterface<User>;
+export type UsersRepositoryInterface = BaseRepositoryInterface<User>;
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,34 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { BaseServiceAbstract } from 'src/services/base/base.abstract.service';
 import { User } from './entities/user.entity';
-import { UserRepositoryInterface } from './interfaces/users.interface';
+import { UsersRepositoryInterface } from './interfaces/users.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRolesService } from '@modules/user-roles/user-roles.service';
+import { USER_ROLE } from '@modules/user-roles/entities/user-role.entity';
 
 @Injectable()
 export class UsersService extends BaseServiceAbstract<User> {
 	constructor(
-		@Inject('UserRepositoryInterface')
-		private readonly users_repository: UserRepositoryInterface,
+		@Inject('UsersRepositoryInterface')
+		private readonly users_repository: UsersRepositoryInterface,
+		private readonly user_roles_service: UserRolesService,
 	) {
 		super(users_repository);
+	}
+
+	async create(create_dto: CreateUserDto): Promise<User> {
+		let user_role = await this.user_roles_service.findOneByCondition({
+			name: USER_ROLE.USER,
+		});
+		if (!user_role) {
+			user_role = await this.user_roles_service.create({
+				name: USER_ROLE.USER,
+			});
+		}
+		const user = await this.users_repository.create({
+			...create_dto,
+			role: user_role,
+		});
+		return user;
 	}
 }
