eebbad332d0ae682166b279b0ad188cd0f018f71
Twelve <ntngocwd96@gmail.com>
2023-04-12 06:45:30 +0700

    improve(base): change _id to id to use with class-transformer

--- src/modules/shared/base/base.entity.ts
+++ src/modules/shared/base/base.entity.ts
@@ -1,9 +1,10 @@
 import { Prop } from '@nestjs/mongoose';
-import { Transform } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class BaseEntity {
-	@Transform((value) => value.obj._id.toString())
-	_id?: string;
+	@Expose()
+	@Transform((value) => value.obj?._id?.toString(), { toClassOnly: true })
+	id?: string;
 
 	@Prop({ default: null })
 	deleted_at?: Date;
