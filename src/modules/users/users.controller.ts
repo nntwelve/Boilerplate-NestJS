5437f30868d4c8b76143c6c216868cf409e3c790
Twelve <ntngocwd96@gmail.com>
2023-04-12 06:46:17 +0700

    docs(user): add example using SerializeOptions ignore response property

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
 	Param,
 	Delete,
 	UseInterceptors,
+	SerializeOptions,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -24,6 +25,9 @@ export class UsersController {
 		return this.users_service.create(create_user_dto);
 	}
 
+	@SerializeOptions({
+		excludePrefixes: ['first', 'last'],
+	})
 	@Get()
 	findAll() {
 		return this.users_service.findAll();
