40145011b2bf4a05a5775d32c2940c83f49dc9b6
Twelve <ntngocwd96@gmail.com>
2023-04-24 14:10:18 +0700

    feat(docs): add swagger api docs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,7 +4,15 @@ import { LocalAuthGuard } from './guards/local.guard';
 import { RequestWithUser } from 'src/types/requests.type';
 import { JwtRefreshTokenGuard } from './guards/jwt-refresh-token.guard';
 import { SignUpDto } from './dto/sign-up.dto';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+	ApiBadRequestResponse,
+	ApiBody,
+	ApiConflictResponse,
+	ApiCreatedResponse,
+	ApiOperation,
+	ApiResponse,
+	ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -47,6 +55,76 @@ export class AuthController {
 			},
 		},
 	})
+	@ApiCreatedResponse({
+		description: 'User created successfully!!',
+		content: {
+			'application/json': {
+				examples: {
+					created_user: {
+						summary: 'Response after sign up',
+						value: {
+							access_token:
+								'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQ0MWNkNmJlMWQ0ZTBiNDRjNzA3NDk2IiwiaWF0IjoxNjgyMDM0MDI3LCJleHAiOjE2ODIwMzc2Mjd9.AH4z7uDWuEDjOs8sesB0ItxKUJ2M3rjul1D1mmjAKieOZblej5mp0JQE5IdgB9LlAOzOtKOLL5RWhxLCZ-YskvoRA7Yqza_rOjfIHeNseC3M66kKYqORN07aZDiA2OWhT3pXBqoKRCUBQCKLgMCAPT-CHryc0wUQGaKxP8YJO8dwIhGtjADchmzNJVBs4G7qYnpZAsORayd5GNfgoLpWmVFIBHSnPLNIL4dL8dLof0GBmVhdjhnHIUXYQlqL1wiwsmxmUC9TU2uiChm-TAhuiQyVwFokSySBJzBrLmEtgy89aaR0YizFK-QMg2xW3cJiiRzEBigTdsR0kvdUlk5GOg',
+							refresh_token:
+								'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQ0MWNkNmJlMWQ0ZTBiNDRjNzA3NDk2IiwiaWF0IjoxNjgyMDM0MDI3LCJleHAiOjE2ODIwNTkyMjd9.aKNZymKdf3VEbPkda2cYYTS7KlpCbTqdXP30LREQ2b_fJ8q8cA0OyNEARK3Jm5yGsKoNd3txi54XmEbf19LC9CuDf9kwgLasPizEeMZsAJqSbSguzE4-9b4sSdf22GyipCcZJpkXkp01Bew04J8Y4FqhNARONsWzySXg8_VVWOGkfHGJVHFs7xYyVvmt3RErJwRM5s1Ou1ok7VW62FSTSAvXw6-qsHp5T7kXo73jECBqSuNEs5JcdluoBjdaAxggHYaDgTXoRh7y4Mn_fVKCQarAsUAxg6w0fxc8Gj0nP1ct3-GjG-Of-0O-iF7uynI2Lnq_On7WUsH7rFSysNyHUg',
+						},
+					},
+				},
+			},
+		},
+	})
+	@ApiBadRequestResponse({
+		description: 'Validation failed',
+		content: {
+			'application/json': {
+				examples: {
+					invalid_email_password: {
+						value: {
+							statusCode: 400,
+							message: [
+								'email must be an email',
+								'password is not strong enough',
+							],
+							error: 'Bad Request',
+						},
+					},
+					some_fields_missing: {
+						value: {
+							statusCode: 400,
+							message: [
+								'last_name must be shorter than or equal to 50 characters',
+								'last_name should not be empty',
+							],
+							error: 'Bad Request',
+						},
+					},
+				},
+			},
+		},
+	})
+	@ApiConflictResponse({
+		description: 'Conflict user info',
+		content: {
+			'application/json': {
+				examples: {
+					email_duplication: {
+						value: {
+							statusCode: 409,
+							message: 'Email already existed!!',
+							error: 'Conflict',
+						},
+					},
+					username_duplication: {
+						value: {
+							statusCode: 409,
+							message: 'Username already existed!!',
+							error: 'Conflict',
+						},
+					},
+				},
+			},
+		},
+	})
 	async signUp(@Body() sign_up_dto: SignUpDto) {
 		return await this.auth_service.signUp(sign_up_dto);
 	}
@@ -71,6 +149,19 @@ export class AuthController {
 					email: 'michaelsmith@example.com',
 					password: '1232@asdS',
 				} as SignUpDto,
+			},
+		},
+	})
+	@ApiResponse({
+		status: 401,
+		description: 'Unauthorized',
+		content: {
+			'application/json': {
+				example: {
+					statusCode: 400,
+					message: 'Wrong credentials!!',
+					error: 'Bad Request',
+				},
 			},
 		},
 	})
